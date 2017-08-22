f84c3861ee605257c2347fe4ad7ab77cde65cd1b
farrelmr <farrelmr@gmail.com>
2017-08-22 21:01:40 +0100

    fdfdsa

--- java-project/src/main/java/com/javabullets/Winner.java
+++ java-project/src/main/java/com/javabullets/Winner.java
@@ -20,6 +20,9 @@ public class Winner {
             new Winner(2015, "Great Britain", "Chris Froome", "Team Sky", 3360, Duration.parse("PT84H46M14S"), 16),
             new Winner(2016, "Great Britain", "Chris Froome", "Team Sky", 3529, Duration.parse("PT89H04M48S"), 14 ));
     
+	public static void main(String[] args) {
+		Winner.runExamples();
+	}
 	public static void runExamples() {
         // Filter and Map -
         List<String> winnersOfToursLessThan3500km = tdfWinners
